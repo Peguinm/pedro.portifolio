bca3d6a7446e4fe285afaf65ac714508482135bc
Peguinm <126339520+Peguinm@users.noreply.github.com>
2024-05-09 17:26:45 -0300

    card links in development

--- src/script.js
+++ src/script.js
@@ -1,4 +1,33 @@
 
-$(function(){
-    $('.disable-section')
+function openCard(cardId){
+    $(cardId).addClass("card-full").css("width", "50%");
+    $(".container-cards > " + cardId + " > .text-box > .hidden-content").css({"max-height": "100%", "display": "block"});
+}
+
+
+function closeCard(cardId){
+    $(".container-cards > " + cardId + " > .text-box > .hidden-content").css({"max-height": "0%", "display": "none"});
+    $(cardId).removeClass("card-full").css("width", "40%");
+}
+
+function onClickCard(btnId, cardId){
+    if($(btnId).text() == "More"){
+        $(btnId).text("Less");
+        openCard(cardId);
+    }else{
+        $(btnId).text("More");
+        closeCard(cardId);
+    }
+}
+
+
+$("#btn-aboutme").on("click", function(){
+    onClickCard("#btn-aboutme", "#card-aboutme");
 });
+
+$("#btn-projects").on("click", function(){
+    onClickCard("#btn-projects", "#card-projects");
+});
+
+
+

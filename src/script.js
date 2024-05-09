63cc3dd77f702c62e2e657233762af265ff7eb3b
unknown <ARS_USER_B03L4_62@lab.uniararas.br>
2024-05-09 20:32:05 -0300

    development

--- src/script.js
+++ src/script.js
@@ -1,32 +1,14 @@
 
-function openCard(cardId){
-    $(cardId).addClass("card-full").css("width", "50%");
-    $(".container-cards > " + cardId + " > .text-box > .hidden-content").css({"max-height": "100%", "display": "block"});
+activeSection = (sectionId) =>{
+    $(sectionId).css("display", "block");
 }
-
-
-function closeCard(cardId){
-    $(".container-cards > " + cardId + " > .text-box > .hidden-content").css({"max-height": "0%", "display": "none"});
-    $(cardId).removeClass("card-full").css("width", "40%");
-}
-
-function onClickCard(btnId, cardId){
-    if($(btnId).text() == "More"){
-        $(btnId).text("Less");
-        openCard(cardId);
-    }else{
-        $(btnId).text("More");
-        closeCard(cardId);
-    }
-}
-
 
 $("#btn-aboutme").on("click", function(){
-    onClickCard("#btn-aboutme", "#card-aboutme");
+    activeSection("#section-aboutme")
 });
 
 $("#btn-projects").on("click", function(){
-    onClickCard("#btn-projects", "#card-projects");
+ 
 });
 
 
